6de461c42784ebea856101456dda42aa8c0c73a8
jbblanton <jessicabblanton@gmail.com>
2020-05-11 23:21:23 +0000

    FutherStudy - done

--- desserts.py
+++ desserts.py
@@ -16,7 +16,7 @@ class Cupcake():
       self.flavor = flavor
       self.price = price
       self.qty = 0
-      # Cupcake.cache[self.name] = Cupcake(name, flavor, price)
+      Cupcake.cache[self.name] = self
 
 
     def add_stock(self, amount):
@@ -29,9 +29,11 @@ class Cupcake():
       """Adjust inventory when selling a cupcake"""  
 
       if self.qty == 0:
-        print("So sorry! There's no more of this cupcake available!")
+        print("Sorry, these cupcakes are sold out")
       elif (self.qty - amount) < 0:
         self.qty = 0
+      else:
+        self.qty -= amount
         ## Should there be an added step, if ordering 10, but only 7 avail, to
         ## give partial orders?
 
@@ -40,15 +42,38 @@ class Cupcake():
     def scale_recipe(ingredients, amount):
       """Adjust ingredient list for different-sized batches"""
 
-      scaled = [qty * amount for ingredient, qty in ingredients]
+      scaled = []
+      for item in ingredients:
+        ing = (item[0], (item[1] * amount))
+        scaled.append(ing)
 
       return scaled
+
+
+    @classmethod
+    def get(cls, name):
+      """Tells if cupcake does/does not exist in cache"""
+
+      if name in cls.cache.keys():
+        return cls.cache.get(name)
+      elif KeyError:
+        print('Sorry, that cupcake doesn\'t exist')
 
 
     def __repr__(self):
         """Human-readable printout for debugging."""
 
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
+
+
+class Brownie(Cupcake):
+  """A subclass for tracking brownies at the bakery"""
+
+  def __init__(name, price):
+
+    super().__init__('brownie', 'chocolate', price)
+      
+
 
 
 if __name__ == '__main__':
