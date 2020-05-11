7c0298af33ca25a9861fb1f4411e4e2a023a3a8f
jbblanton <jessicabblanton@gmail.com>
2020-05-11 19:36:26 +0000

    Halfway

--- desserts.py
+++ desserts.py
@@ -1,8 +1,49 @@
 """Dessert classes."""
 
 
-class Cupcake:
+class Cupcake():
     """A cupcake."""
+
+    # To store all types of cupcake, by name
+    cache = {}
+
+
+
+    def __init__(self, name, flavor, price):
+      """Cupcake object"""
+
+      self.name = name
+      self.flavor = flavor
+      self.price = price
+      self.qty = 0
+      # Cupcake.cache[self.name] = Cupcake(name, flavor, price)
+
+
+    def add_stock(self, amount):
+      """Adjust inventory when baking a cupcake"""
+ 
+      self.qty += amount
+
+
+    def sell(self, amount):
+      """Adjust inventory when selling a cupcake"""  
+
+      if self.qty == 0:
+        print("So sorry! There's no more of this cupcake available!")
+      elif (self.qty - amount) < 0:
+        self.qty = 0
+        ## Should there be an added step, if ordering 10, but only 7 avail, to
+        ## give partial orders?
+
+
+    @staticmethod
+    def scale_recipe(ingredients, amount):
+      """Adjust ingredient list for different-sized batches"""
+
+      scaled = [qty * amount for ingredient, qty in ingredients]
+
+      return scaled
+
 
     def __repr__(self):
         """Human-readable printout for debugging."""
